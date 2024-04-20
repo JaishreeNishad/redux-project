1a5be3740d22d1aa7ea662a7ca5e934c46bf68bf
JaishreeNishad <Jaysh2222@gmail.com>
2024-04-20 15:46:05 +0530

    Apply data fetching using async await

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+
+      const data = await res.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div className="productsWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="" />
+          <h4>{product.title}</h4>
+          <h4>{product.price}</h4>
+          <button className="btn">Add to cart</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
