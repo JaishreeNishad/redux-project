aca0b4135f53ee0a2a9a0fb8b7c8c64b672851f6
JaishreeNishad <Jaysh2222@gmail.com>
2024-04-22 13:14:24 +0530

    apply logic for remove cart item

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleRemove = (productId) => {
+    dispatch(remove(productId));
+  };
   return (
     <div>
       <h3>Cart</h3>
@@ -12,7 +18,9 @@ const Cart = () => {
             <img src={products.image} alt="" />
             <h5>{products.title}</h5>
             <h5>{products.price}</h5>
-            <button className="btn">Remove</button>
+            <button className="btn" onClick={() => handleRemove(products.id)}>
+              Remove
+            </button>
           </div>
         ))}
       </div>
